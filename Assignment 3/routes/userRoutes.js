6b8b3acd60d646d5275e35ca053525cd1953930e
ZD <zhongda2525@gmail.com>
2024-11-21 22:41:14 +0800

    http method change to put

--- Assignment 3/routes/userRoutes.js
+++ Assignment 3/routes/userRoutes.js
@@ -5,6 +5,6 @@ const apis = require("../api.js")
 // Assignment 3
 router.post("/CreateTask", apis.CreateTaskController)
 router.post("/GetTaskByState", apis.GetTaskByStateController)
-router.patch("/PromoteTask2Done", apis.PromoteTask2DoneController)
+router.put("/PromoteTask2Done", apis.PromoteTask2DoneController)
 
 module.exports = router
